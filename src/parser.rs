e2c36c5ba833b4a3fc8c2b328f5b07f6e59bf17d
LunarTides <31688109+LunarTides@users.noreply.github.com>
2024-09-11 13:15:39 +0200

    Remove leading zeroes on numbers and add `pop_token` helper

--- src/parser.rs
+++ src/parser.rs
@@ -1,8 +1,8 @@
-use crate::lexer::{LexedTokens, Token};
+use crate::lexer::{LexedToken, LexedTokens, Token};
 
 #[derive(Default)]
 pub struct Parser {
-    stack: Vec<String>,
+    stack: Vec<LexedToken>,
 }
 
 impl Parser {
@@ -18,7 +18,7 @@ impl Parser {
             for token in tokens {
                 match token.1 {
                     Token::Identifier => self.parse_identifier(token.0),
-                    Token::Number | Token::String => self.stack.push(token.0),
+                    Token::Number | Token::String => self.stack.push(token),
                 }
             }
         }
@@ -28,13 +28,36 @@ impl Parser {
 
     fn parse_identifier(&mut self, identifier: String) {
         if identifier == "print" {
-            println!("{}", self.stack.last().unwrap());
-            self.stack.pop();
-        } else if identifier == "add" {
-            let a = self.stack.pop().unwrap().parse::<i64>().unwrap();
-            let b = self.stack.pop().unwrap().parse::<i64>().unwrap();
+            let token = self.pop_token("print", 1, Some(("".into(), Token::String)));
+            let mut to_print = &token.0;
 
-            self.stack.push((a + b).to_string());
+            // Remove leading zeroes if the value is a number.
+            let to_print_if_number = &to_print.trim_start_matches('0').to_string();
+
+            if token.1 == Token::Number {
+                to_print = to_print_if_number
+            }
+
+            println!("{}", to_print);
+        } else if identifier == "add" {
+            let a = self.pop_token("add", 2, None).0.parse::<i64>().unwrap();
+            let b = self.pop_token("add", 2, None).0.parse::<i64>().unwrap();
+
+            self.stack.push(((a + b).to_string(), Token::Number));
         }
+    }
+
+    fn pop_token(
+        &mut self,
+        identifier: &str,
+        expected_argument_amount: u8,
+        default: Option<LexedToken>,
+    ) -> LexedToken {
+        self.stack.pop().or(default).unwrap_or_else(|| {
+            panic!(
+                "`{}` needs {} argument(s).",
+                identifier, expected_argument_amount
+            )
+        })
     }
 }
